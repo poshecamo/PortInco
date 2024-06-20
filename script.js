1bbfecfaf6e5efe64e63ec7a9a611320311efc07
poshecamo <poshecamo@gmail.com>
2024-06-19 20:26:33 -0700

    blog page added, updated css

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.getElementById('external-link-button').addEventListener('click', function() {
     window.open('https://www.instagram.com/apollynary/', '_blank');
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const blogPosts = document.querySelectorAll(".blog-post");
+
+    blogPosts.forEach(post => {
+        const summary = post.querySelector(".blog-summary");
+        
+        summary.addEventListener("click", () => {
+            post.classList.toggle("active");
+        });
+    });
+});
